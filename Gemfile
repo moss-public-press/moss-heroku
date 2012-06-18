bcfbc32c652ace6025616b50f014073b5c8ad18e
sethvincent <sethvincent@gmail.com>
2012-06-17 17:09:13 -0700

    add form

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source :rubygems
 gem 'sinatra'
+gem 'thin'
+gem 'pony', '1.4'
+gem 'rack-flash'
+gem 'rack', '1.3.6'
--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'rack-flash'
 
 class App < Sinatra::Base
+  use Rack::Flash
+  
+  enable :static, :sessions
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
   
   get '/' do
     erb :index
@@ -11,8 +17,30 @@ class App < Sinatra::Base
     if File.exists?('views/'+params[:page]+'.erb')
       erb params[:page].to_sym
     else 
-      erb :fourohfour
+      flash[:notice] = "huh. whatever you were lokking for doesn't exist"
+      redirect '/'
     end   
+  end
+  
+  post '/' do 
+      require 'pony'
+       Pony.mail(
+        :from => params[:name] + "<" + params[:email] + ">",
+        :to => 'sethvincent@gmail.com',
+        :subject => params[:name] + " has contacted you",
+        :body => params[:message],
+        :port => '587',
+        :via => :smtp,
+        :via_options => { 
+          :address              => 'smtp.sendgrid.net', 
+          :port                 => '587', 
+          :enable_starttls_auto => true, 
+          :user_name            => ENV['SENDGRID_USERNAME'], 
+          :password             => ENV['SENDGRID_PASSWORD'], 
+          :authentication       => :plain, 
+          :domain               => ENV['SENDGRID_DOMAIN']
+        })
+      flash[:notice] = "Oh! Thanks for the email, bud!"
   end
   
 end
